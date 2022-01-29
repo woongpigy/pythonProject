8e0d6c9714d20bb55cb5067a04b2468b56233fc2
woongpigy <jinwoonggg@gmail.com>
2022-01-29 16:12:25 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from operator import getitem
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.datasets import mnist
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import import_ipynb
+
+git clone https://github.com/yhlee1627/deeplearning.get
